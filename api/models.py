506f4864e0e2c249d0e0a48dcaab9d863b8f09e8
Britton W <britton.wolfe1999@gmail.com>
2020-06-26 14:09:39 -0400

    started doing stuff

--- api/models.py
+++ api/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+MAX_FIELD_LEN = 63
 
 class User(AbstractUser):
-    pass
+	pass
+
+class Book(models.Model):
+	user = models.ForeignKey(to = User, on_delete = models.CASCADE, related_name = "books")
+	title = models.CharField(max_length = MAX_FIELD_LEN)
+	author = models.CharField(max_length = MAX_FIELD_LEN)
+	page = models.IntegerField()
+	status = models.IntegerField()
+	start_date = models.DateField(auto_now_add = True)
--- booklist/settings.py
+++ booklist/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'django_extensions',
     'rest_framework',
+    'djoser',
 
     # Project-specific
     'api',
